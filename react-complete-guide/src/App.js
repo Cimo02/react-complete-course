6d8cc479ef7910d8d3f761126c3d580b79a6af1e
Tyler Ciarmataro <tjc3255@g.rit.edu>
2021-02-01 19:04:38 -0500

    converted app to a functional component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,40 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  // personsState is the actual data, and setPersonsState is the function that updates the state
+  // you can use 'useState' as many times as you want in a functional component
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Tyler Ciarmataro', age: 23 },
       { name: 'Meghan Hubbard', age: 22 },
       { name: 'Jamie Ciarmataro', age: 20 },
-    ],
-    otherState: 'some other value',
-  }
+    ]
+  });
   
-  switchName = () => {
-    //console.log("Name was switched!");
+  const [ otherState, setOtherState ] = useState({
+    otherState: 'some other state?',
+  });
+
+  console.log(personsState, otherState);
+
+  const switchName = () => {
+    // console.log("Name was switched!");
     // DON'T DO THIS: this.state.persons[0].name = "Tyler"
-    this.setState({
+    /* When using Hooks you need to copy all information you don't want to delete, 
+      because 'setStateValues' replaces state variables */
+    setPersonsState({
       persons: [
-      { name: 'Tyler', age: 23 },
-      { name: 'Meghan', age: 22 },
-      { name: 'Jamie', age: 20 },
-    ],});
+        { name: 'Tyler', age: 23 },
+        { name: 'Meghan', age: 22 },
+        { name: 'Jamie', age: 20 },
+      ],
+      otherState: personsState.otherState,
+    });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi! I'm a React App!</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchName}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Games/Puzzles</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: TV/Movies</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies: Art</Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi! I'm a React App!</h1>
+      <p>This is really working!</p>
+      <button onClick={switchName}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies: Games/Puzzles</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: TV/Movies</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My Hobbies: Art</Person>
+    </div>
+  );
 }
 
-export default App;
+export default app;
